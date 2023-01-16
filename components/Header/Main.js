4fae2dd8c9809651c1a6884e0cae702470dff676
krishna <devenger@192.168.1.12>
2023-01-16 21:47:08 +0530

    Added Header Mainjs code

--- components/Header/Main.js
+++ components/Header/Main.js
@@ -1,7 +1,24 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
+import { RiSearch2Line } from "react-icons/ri";
+import { FaOpencart } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export default function Main() {
+  // const router = useRouter();
+  // const [query, setQuery] = useState(router.query.search || "");
+  const { cart } = useSelector((state) => ({ ...state }));
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // if (router.pathname !== "/browse") {
+    //   if (query.length > 1) {
+    //     router.push(`/browse?search=${query}`);
+    //   }
+    // } else {
+    //   searchHandler(query);
+    // }
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.main__container}>
@@ -9,6 +26,22 @@ export default function Main() {
           {/* <a className={styles.logo}> */}
           <img src="../../../logo.png" alt="" />
           {/* </a> */}
+        </Link>
+
+        <form onSubmit={(e) => handleSearch(e)} className={styles.search}>
+          <input
+            type="text"
+            placeholder="Search..."
+            // value={query}
+            // onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className={styles.search__icon}>
+            <RiSearch2Line />
+          </button>
+        </form>
+        <Link href="/cart" className={styles.cart}>
+          <FaOpencart />
+          <span>0</span>
         </Link>
       </div>
     </div>
